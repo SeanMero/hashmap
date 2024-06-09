bdf2580502a019fb170c671bcd5c046ace0cf5ea
SeanMero <MeroFromVero@gmail.com>
2024-06-09 16:16:41 +0800

    Add method to generate a new node in the hash map

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,4 +1,13 @@
-class HashMap
+class HashMap < Array
+  attr_accessor :capacity, :load_factor, :num_keys
+
+  def initialize(capacity = 16, load_factor = 0.75)
+    super
+    @capacity = capacity
+    @load_factor = load_factor
+    @num_keys = 0
+  end
+
   def hash(key)
     hash_code = 0
     prime_number = 31
@@ -6,5 +15,18 @@ class HashMap
     key.each_char { |char| hash_code = prime_number * hash_code + char.ord }
 
     hash_code
+  end
+
+  def set(key, value)
+    hashed_key = hash(key) % capacity
+    if self[hashed_key].nil?
+      self[hashed_key] = { key => value }
+    else
+      self[hashed_key][key] = value
+    end
+
+    # grow more buckets when needed
+    num_keys += 1
+    capacity *= 2 if num_keys > capacity * load_factor
   end
 end
